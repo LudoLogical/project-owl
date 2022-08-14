70fc910c5e3e30a82679121ff5d174bfaa6d000f
LudoLogical <dcd180001@utdallas.edu>
2022-08-14 12:25:07 -0500

    Gutted boilerplate

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,10 +1,10 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
 }
 
+// noinspection JSUnusedGlobalSymbols
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
